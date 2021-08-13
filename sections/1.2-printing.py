72114268fc8aefc2d956ab7a7a7063ee4aa254c4
Lee Crosby <ligand@hotmail.co.uk>
2021-08-13 22:13:17 +0100

    Update 1.2-printing.py

--- sections/1.2-printing.py
+++ sections/1.2-printing.py
@@ -2,13 +2,18 @@
 """
 
 # %%
+
 print("This line will be printed.")
 
-"""This may seem very trivial, but there was a lot of back and forth in the old days about the
-print-statement and the print-function. Just stick with the print-function
+# %%
+
+"""This may seem very trivial, but there was a lot of back-and-forth in the dark ages about the
+print-statement vs the print-function. Just stick with the print-function!
 """
 
 # %%
+
 print("use this")
 # print "not this"
+
 # %%
