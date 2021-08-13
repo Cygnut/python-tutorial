b2409f6e1efe33e86d512d48bf81484bd74df118
Lee Crosby <ligand@hotmail.co.uk>
2021-08-13 21:59:13 +0100

    Update 0.1-setup.py

--- sections/0.1-setup.py
+++ sections/0.1-setup.py
@@ -1,4 +1,4 @@
-"""Context - what's Python's used for (at least in Feed It Back):
+"""Context - what's Python used for (at least in Feed It Back..):
 - Quick scripts
 - Data analysis
 - Kirsty originally used it in the deploy - largely because of the ease of integration & packages e.g. argparse & ssh.
@@ -6,13 +6,14 @@
 
 """Installation on a Mac
 brew install python3
-alias py=python3
 """
 
 """The simplest way to run the samples is to select a line containing the following sequence:
 """
+
 # %%
 print('hi there!')
+# %%
 
 """In VSCode, you should be able to hit 'Run Cell' or 'Debug Cell'. Installing the necessary
 pre-requisites will let you run/debug that cell in isolation.
