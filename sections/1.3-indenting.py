c2d474157530ca796eb91b99e13fe5f699321432
Lee Crosby <ligand@hotmail.co.uk>
2021-08-13 22:22:22 +0100

    Update 1.3-indenting.py

--- sections/1.3-indenting.py
+++ sections/1.3-indenting.py
@@ -1,19 +1,19 @@
-"""Yes, this thing..
+"""Okay, about the indenting thing..
 
-Python uses indentation for blocks, instead of curly braces. Both tabs and spaces are supported,
-but the standard indentation requires standard Python code to use four spaces. For example:
+Python uses indentation to define blocks and scope, instead of curly braces. An indent
+is defined as 4 spaces.
 """
 
 # %%
+
 x = 1
 if x == 1:
-    # indented four spaces
-    print("x is 1.")
+    print("x is 1, well done.")
 
 # %%
 
 """pass
-You should use pass when in situations where a level of indentiation is required to indicate the
+You should use 'pass' in situations where a level of indentation is required to indicate the
 termination of a block, but you have nothing useful to write. This is usually the case with
 empty methods or classes, e.g.:
 """
@@ -22,6 +22,7 @@ empty methods or classes, e.g.:
 # def some_empty_function():
 
 # %%
+
 def some_empty_function():
     pass
 
