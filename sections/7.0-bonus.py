131ee4c859f745da1ffc43543f7162944b29f09e
Lee Crosby <ligand@hotmail.co.uk>
2021-08-13 23:38:35 +0100

    Update 7.0-bonus.py

--- sections/7.0-bonus.py
+++ sections/7.0-bonus.py
@@ -1,4 +1,4 @@
-"""../_combine.py consists of a script which is run to combine all of these sections into one. It's
+"""../build_cheatsheet.py consists of a script which is run to combine all of these sections into one. It's
 intentionally over-engineered, to illustrate and combine a lot of the concepts covered here - take
 a look!
 """
