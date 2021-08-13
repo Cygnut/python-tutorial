7abc80aa806ea5d24a39cf8b8e1150d125a26f1a
lee <lee@feeditback.co.uk>
2021-08-13 19:02:42 +0100

    good luck!

--- sections/7.0-bonus.py
+++ sections/7.0-bonus.py
@@ -1,4 +1,6 @@
 """../_combine.py consists of a script which is run to combine all of these slides into one. It's
 intentionally over-engineered, to illustrate and combine a lot of the concepts covered here - take
 a look.
+
+Good luck!
 """
