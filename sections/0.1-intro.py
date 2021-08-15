39f8b6a0070c1de260f56aa4783ddd0a4b00a29c
Lee Crosby <ligand@hotmail.co.uk>
2021-08-15 16:25:24 +0100

    Update 0.1-intro.py

--- sections/0.1-intro.py
+++ sections/0.1-intro.py
@@ -42,37 +42,3 @@ pre-requisites will let you run/debug that cell in isolation.
 Failing that, enter the python REPR in your terminal (just run `python`), and either copy and
 paste the samples, or select your code -> then hit `Shift + Return` or just hit run to run it all
 """
-
-"""Some quick examples:
-"""
-
-"""Here's how 'Hello World!' is spelt in Python:
-"""
-
-# %%
-
-print('Hello World!')
-
-# %%
-
-"""And FizzBuzz:
-Write a program that prints the numbers 1-100, each on a new line.
-- For each number that is a multiple of 3, print "Fizz" instead of the number.
-- For each number that is a multiple of 5, print "Buzz" instead of the number.
-- For each number that is a multiple of both 3 and 5, print "FizzBuzz" instead of the number.
-"""
-
-# %%
-
-def fizzbuzz():
-    for i in range(1, 101):
-        s = ''
-        if i % 3 == 0:
-            s += 'Fizz'
-        elif i % 5 == 0:
-            s += 'Buzz'
-        print(s if s else i)
-
-fizzbuzz()
-      
-# %%
