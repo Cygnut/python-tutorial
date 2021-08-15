011e15c882eccf5297947c96c8facf24b44b0625
Cygnut <ligand@hotmail.co.uk>
2021-08-15 16:38:36 +0100

    pythagoras

--- sections/0.1-intro.py
+++ sections/0.1-intro.py
@@ -26,19 +26,3 @@ Okey doke!
 - Data analysis
 - Kirsty originally used it in the deploy - largely because of the ease of integration & packages e.g. argparse & ssh.
 """
-
-"""The simplest way to run the samples is to select a line containing the following sequence:
-"""
-
-# %%
-
-print('hi there!')
-
-# %%
-
-"""In VSCode, you should be able to hit 'Run Cell' or 'Debug Cell'. Installing the necessary
-pre-requisites will let you run/debug that cell in isolation.
-
-Failing that, enter the python REPR in your terminal (just run `python`), and either copy and
-paste the samples, or select your code -> then hit `Shift + Return` or just hit run to run it all
-"""
---
+++ sections/0.2-setup.py
@@ -0,0 +1,15 @@
+"""The simplest way to run the samples is to select a line containing the following sequence: # %%
+
+In VSCode, you should be able to hit 'Run Cell' or 'Debug Cell'. Installing the necessary
+pre-requisites will let you run/debug that cell in isolation.
+"""
+
+# %%
+
+print('hi there!')
+
+# %%
+
+"""(Failing that, enter the python REPR in your terminal (just run `python`), and either copy and
+paste the samples, or select your code -> then hit `Shift + Return` or just hit run to run it all)
+"""
