9e15e670aae284aba17e7e9821d693d555432637
Lee Crosby <ligand@hotmail.co.uk>
2021-08-15 16:26:07 +0100

    Create 0.2-katas.py

---
+++ sections/0.2-katas.py
@@ -0,0 +1,34 @@
+
+"""Some common katas in Python:
+"""
+
+"""Here's how 'Hello World!' is spelt in Python:
+"""
+
+# %%
+
+print('Hello World!')
+
+# %%
+
+"""FizzBuzz:
+Write a program that prints the numbers 1-100, each on a new line.
+- For each number that is a multiple of 3, print "Fizz" instead of the number.
+- For each number that is a multiple of 5, print "Buzz" instead of the number.
+- For each number that is a multiple of both 3 and 5, print "FizzBuzz" instead of the number.
+"""
+
+# %%
+
+def fizzbuzz():
+    for i in range(1, 101):
+        s = ''
+        if i % 3 == 0:
+            s += 'Fizz'
+        elif i % 5 == 0:
+            s += 'Buzz'
+        print(s if s else i)
+
+fizzbuzz()
+      
+# %%
