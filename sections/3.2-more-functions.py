9a6676f9b34d3151749ace1b3d8ba509513ba536
lee <lee@feeditback.co.uk>
2021-08-19 15:38:58 +0100

    first class citizens

--- sections/3.2-more-functions.py
+++ sections/3.2-more-functions.py
@@ -46,3 +46,33 @@ def bar(first, second, third, **kwargs):
 bar(1, 2, 3, action="sum", number="first")
 
 # %%
+
+"""Functions (as with most definitions in Python) are first-class citizens, which is a big
+simplification in a lot of situations..
+"""
+
+# %%
+
+
+def old_function(num):
+    return 10 * num
+
+
+# Create an alias for new_function, for folks with ageism..
+new_function = old_function
+
+print(new_function(50) == old_function(50))
+
+# Try and import a function, falling back it if it does not exist:
+try:
+    from os import some_non_existing_function
+except ImportError:
+
+    def some_non_existing_function():
+        print(
+            "Using the fallback, rather than something which doesn't exist in the os module"
+        )
+
+
+some_non_existing_function()
+# %%
