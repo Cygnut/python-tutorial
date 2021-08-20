458e6f8d2ab929e6597e3810f7db290638affd4c
lee <lee@feeditback.co.uk>
2021-08-20 11:27:27 +0100

    generic

--- sections/0.1-intro.py
+++ sections/0.1-intro.py
@@ -1,28 +1,22 @@
 """What is Python?
 Paraphrasing from https://www.python.org/doc/essays/blurb/:
 
-  Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level 
+  Python is an interpreted, object-oriented, high-level programming language with dynamic semantics. Its high-level
   built in data structures, combined with dynamic typing and dynamic binding...
 
-  Often, programmers fall in love with Python because of the increased productivity it provides. Since there is no 
-  compilation step, the edit-test-debug cycle is incredibly fast. Debugging Python programs is easy: a bug or bad 
+  Often, programmers fall in love with Python because of the increased productivity it provides. Since there is no
+  compilation step, the edit-test-debug cycle is incredibly fast. Debugging Python programs is easy: a bug or bad
   input will never cause a segmentation fault.
 
-  Instead, when the interpreter discovers an error, it raises an exception. When the program doesn't catch the 
-  exception, the interpreter prints a stack trace. 
+  Instead, when the interpreter discovers an error, it raises an exception. When the program doesn't catch the
+  exception, the interpreter prints a stack trace.
 
-  A source level debugger allows inspection of local and global variables, evaluation of arbitrary expressions, 
-  setting breakpoints, stepping through the code a line at a time, and so on. The debugger is written in Python 
-  itself, testifying to Python's introspective power. 
+  A source level debugger allows inspection of local and global variables, evaluation of arbitrary expressions,
+  setting breakpoints, stepping through the code a line at a time, and so on. The debugger is written in Python
+  itself, testifying to Python's introspective power.
 
-  On the other hand, often the quickest way to debug a program is to add a few print statements to the source: the 
+  On the other hand, often the quickest way to debug a program is to add a few print statements to the source: the
   fast edit-test-debug cycle makes this simple approach very effective.
 
 Okey doke!
-"""
-
-"""Context - what's Python used for (at least in Feed It Back..):
-- Quick scripts
-- Data analysis
-- Kirsty originally used it in the deploy - largely because of the ease of integration & packages e.g. argparse & ssh.
 """
